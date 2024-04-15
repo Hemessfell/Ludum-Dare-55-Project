9294e31318161d1f5060a3b872bb95fb978f6467
Gustavo Maia <74977338+Hemessfell@users.noreply.github.com>
2024-04-15 10:47:31 -0300

    Salve

--- Ludum Dare 55 Project/Assets/UI/Scripts/ManaFill.cs
+++ Ludum Dare 55 Project/Assets/UI/Scripts/ManaFill.cs
@@ -8,10 +8,16 @@ public class ManaFill : MonoBehaviour
     [SerializeField, Range(0, 1)] private float manaAmmount;
 
     private float initialHeight;
+    private bool reachedZero;
 
     private void Awake()
     {
         initialHeight = rectTransform.sizeDelta.y;
+    }
+
+    private void Update()
+    {
+        rectTransform.sizeDelta = new Vector3(rectTransform.sizeDelta.x, Mathf.Lerp(0.0f, initialHeight, manaAmmount));
     }
 
     public void DecreaseMana(float decreaseValue)
@@ -19,6 +25,11 @@ public class ManaFill : MonoBehaviour
         manaAmmount -= decreaseValue;
         manaAmmount = Mathf.Clamp01(manaAmmount);
         rectTransform.sizeDelta = new Vector3(rectTransform.sizeDelta.x, Mathf.Lerp(0.0f, initialHeight, manaAmmount));
+
+        if(manaAmmount == 0.0f)
+        {
+            reachedZero = true;
+        }
     }
 
     public void IncreaseMana(float increaseValue)
@@ -26,5 +37,6 @@ public class ManaFill : MonoBehaviour
         manaAmmount += increaseValue;
         manaAmmount = Mathf.Clamp01(manaAmmount);
         rectTransform.sizeDelta = new Vector3(rectTransform.sizeDelta.x, Mathf.Lerp(0.0f, initialHeight, manaAmmount));
+        reachedZero = false;
     }
 }
