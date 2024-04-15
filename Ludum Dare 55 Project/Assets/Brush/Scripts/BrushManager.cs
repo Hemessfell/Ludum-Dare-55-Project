cb406f1315f00642255a0eb78a565d005f3b2867
haradakk <haradakk@hotmail.com>
2024-04-15 12:17:39 -0300

    w

--- Ludum Dare 55 Project/Assets/Brush/Scripts/BrushManager.cs
+++ Ludum Dare 55 Project/Assets/Brush/Scripts/BrushManager.cs
@@ -1,44 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEngine.UI;
 
 public class BrushManager : MonoBehaviour
 {
-    public enum States {Green, Red, Yellow}
+    public enum States { Green, Red, Yellow }
     [SerializeField] private Line line;
     [SerializeField] private GameObject wall;
+    [SerializeField] private List<GameObject> wallList = new List<GameObject>();
     [SerializeField] private LayerMask whatIsObstacle;
+    [SerializeField] private float greenMana, redMana, yellowMana;
+    private Image manaColor;
     private LineRenderer lineRenderer;
     private States myState;
 
+    [SerializeField] private ManaFill mana;
     private void Awake()
     {
         lineRenderer = line.GetComponent<LineRenderer>();
+        manaColor = mana.GetComponent<Image>();
     }
     void Start()
     {
-        myState = States.Green;
+        myState = States.Red;
+        SwitchState(States.Green);
     }
 
-    
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             SwitchState(States.Green);
         }
-        
-        if(Input.GetKeyDown(KeyCode.R))
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SwitchState(States.Yellow);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
             SwitchState(States.Red);
 
         switch (myState)
         {
-            
+
             case States.Green:
                 if (Input.GetMouseButton(0))
                     SpawnWall();
+                if (Input.GetMouseButtonUp(0))
+                    StartCoroutine(ActiveWall());
                 return;
 
             case States.Red:
@@ -51,14 +62,31 @@ public class BrushManager : MonoBehaviour
 
     private void SpawnWall()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit;
-        hit = Physics2D.Raycast(new Vector3(mousePosition.x, mousePosition.y, 0), Vector2.down,0.1f, whatIsObstacle);
-       
-        if(!hit)
+        Vector3 _mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D _hit;
+        _hit = Physics2D.BoxCast(new Vector3(_mousePosition.x, _mousePosition.y, 0), new Vector2(1, 0.625f), 0, Vector2.down, 0.1f, whatIsObstacle);
+
+        if (!_hit)
         {
-            //Instantiate(wall,new Vector3 (mousePosition.x, mousePosition.y,0),Quaternion.identity,transform);
-            Instantiate(wall, new Vector3(Mathf.FloorToInt(mousePosition.x) + 0.5f, Mathf.RoundToInt(mousePosition.y), 0), Quaternion.identity, transform);
+            Instantiate(wall,new Vector3 (_mousePosition.x, _mousePosition.y,0),Quaternion.identity,transform);
+            mana.DecreaseMana(greenMana);
+            //Instantiate(wall, new Vector3(Mathf.FloorToInt(mousePosition.x) + 0.5f, Mathf.RoundToInt(mousePosition.y), 0), Quaternion.identity, transform);
+            GameObject _temp = Instantiate(wall,new Vector3 (_mousePosition.x, _mousePosition.y,0),Quaternion.identity,transform);
+            wallList.Add(_temp);
+        }
+    }
+
+    private IEnumerator ActiveWall()
+    {
+        if(wallList.Count > 0)
+        {
+            for (int i = 0; i < wallList.Count; i++)
+            {
+
+                wallList[i].transform.GetChild(0).gameObject.SetActive(true);
+                yield return new WaitForSeconds(0.05f);
+            }
+            wallList.Clear();
         }
     }
 
@@ -67,20 +95,23 @@ public class BrushManager : MonoBehaviour
         myState = newState;
         if (myState is States.Green)
         {
-            lineRenderer.startColor = new Color(0.1779548f, 0.4056604f,0.2083156f);
-
+            lineRenderer.startColor = new Color(0.1779548f, 0.4056604f, 0.2083156f);
+            manaColor.color = Color.green;
             lineRenderer.endColor = new Color(0, 0.1415094f, 0.02979144f);
         }
 
         if (newState is States.Red)
         {
-            lineRenderer.startColor = new Color(140, 24, 3);
-            Debug.Log("red");
-            lineRenderer.endColor = new Color(198, 94, 27);
+            lineRenderer.startColor = new Color(0.5471698f, 0.09292281f, 0.01290495f);
+            manaColor.color = Color.red;
+            lineRenderer.endColor = new Color(0.7764707f, 0.3686275f, 0.1058824f);
         }
         else if (newState is States.Yellow)
         {
-
+            lineRenderer.startColor = new Color(0.6313726f, 0.6078432f, 0.4705883f);
+            manaColor.color = Color.yellow;
+            lineRenderer.endColor = new Color(0.9960785f, 0.9058824f, 0.3803922f);
         }
     }
+
 }
--- Ludum Dare 55 Project/Assets/Enemies/Script/TurretEnemy.cs
+++ Ludum Dare 55 Project/Assets/Enemies/Script/TurretEnemy.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class TurretEnemy : MonoBehaviour
 {
-    [SerializeField] private float shootInterval;
+    [SerializeField] private float shootInterval, radius;
     [SerializeField] private GameObject projectile;
-    [SerializeField] private CircleCollider2D circleCollider2D;
-    private bool recharge;
+    private bool recharge,isplayerIn;
     private Coroutine coroutine;
+    [SerializeField] private LayerMask whatIsPlayer;
 
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void Update()
+    {
+        isplayerIn = Physics2D.OverlapCircle(transform.position, radius, whatIsPlayer);
+        if(isplayerIn && !recharge)
+        {
+            coroutine = StartCoroutine(Shoot());
+        }
+    }
+  /*  private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Wagon") && !recharge)
         {
@@ -24,12 +32,17 @@ public class TurretEnemy : MonoBehaviour
         {
             StopCoroutine(coroutine);
         }
-    }
+    }*/
     private IEnumerator Shoot()
     {
         recharge = true;
         yield return new WaitForSeconds(shootInterval);
         Instantiate(projectile, transform.position, Quaternion.identity);
         recharge = false;
+    }
+    public  void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
